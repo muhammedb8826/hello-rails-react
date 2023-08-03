a1ccaae30dfc9e350f492bac722905dbeb2eb475
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-08-04 02:07:04 +0300

    Add A webpack

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,7 +1,4 @@
 class RootController < ApplicationController
   def index
   end
-  def greeting
-    @messages = Message.all
-  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-  return (<h1>Hey Bahir</h1>);
+  return (<h1>Hey Muhammed</h1>);
 }
 
 ReactDOM.render(
